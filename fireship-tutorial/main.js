cafab969ea4700cdf42ac1eec00efd88e26bc92c
Styn Vercauteren <styn.vercauteren@outlook.com>
2025-04-21 09:52:33 +0200

    Change lighting

--- fireship-tutorial/main.js
+++ fireship-tutorial/main.js
@@ -18,16 +18,27 @@ renderer.setPixelRatio(window.devicePixelRatio)
 renderer.setSize(window.innerWidth, window.innerHeight)
 camera.position.setZ(30)
 
-renderer.render(scene, camera)
+// renderer.render(scene, camera)
 
 const geometry = new THREE.TorusGeometry(10, 3, 16, 100)
-const material = new THREE.MeshBasicMaterial({ color: 0xFF6347, wireframe: true })
+const material = new THREE.MeshStandardMaterial({ color: 0xFF6347 })
 const torus = new THREE.Mesh(geometry, material)
-
 scene.add(torus)
+
+const pointLight = new THREE.PointLight(0xffffff)
+pointLight.position.set(20, 20, 20)
+// scene.add(pointLight)
+
+const ambientLight = new THREE.AmbientLight(0xffffff)
+scene.add(pointLight, ambientLight)
 
 const animate = () => {
     requestAnimationFrame(animate)
+
+    torus.rotation.x += 0.005
+    torus.rotation.y += 0.005
+    torus.rotation.z += 0.005
+    
     renderer.render(scene, camera)
 }
 
